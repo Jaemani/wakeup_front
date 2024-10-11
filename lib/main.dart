6bf38f95ed397d152e93b2d60ba6f704f27d0d1a
Jaemani <leejaeman0227@gmail.com>
2024-10-11 17:20:13 +0900

    firebase function added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:audioplayers/audioplayers.dart';
+
+final callable = FirebaseFunctions.instance.httpsCallable('hashApiKey');
+final result = await callable.call({'apiKey': userApiKey});
+final hashedKey = result.data['hashedKey'];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
